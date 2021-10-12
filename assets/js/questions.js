cb0d78620a5d548c11b075ce4ac533f181ca3ee9
Arkay <riyadh.k@outlook.com>
2021-10-12 13:58:17 +0000

    Add structure to html file and questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 2,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 3,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 3,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 4,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 5,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 6,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+
+  },
+];
