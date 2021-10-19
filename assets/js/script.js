b2b4de9ff4f146e2daf659f10ad8644b18ea0049
Arkay <riyadh.k@outlook.com>
2021-10-19 16:21:38 +0000

    Declare buttons needed and implement onClick for continue button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
-const continue_btn = document.getElementById("continue");
-const exit_btn = document.querySelector(".quit buttons back-card-button");
+/* select all elements for main divs */
+const flipcard = document.querySelector(".maincontainer");
+const quizBox = document.querySelector(".quiz_box");
+const resultBox = document.querySelector(".result_box");
+
+/* select the required buttons */
+const start_btn = document.getElementById("continue");
+const next_btn = quizBox.querySelector("next-btn");
+const option_list = document.querySelectorAll(".option");
+const clear_button = document.querySelector(".clear buttons");
+const replay_quiz = document.querySelector(".replay buttons");
+const exit_btn = document.querySelector(".quit buttons");
+
+const timeText = document.querySelector(".timer .time-passed");
+const timeCount = document.querySelector(".timer .timer-sec");
+
+/* Initialsing / Declaring varibales needed for script */
+let counter;
+
+start_btn.onclick = ()=>{
+    flipcard.classList.add("hidden"); //hide flipcard
+    quizBox.classList.remove("hidden"); //remove hidden from quiz
+}
 
