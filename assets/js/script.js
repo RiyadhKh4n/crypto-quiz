de18e209e7ccfc536496f67fff022f12b7203193
Arkay <riyadh.k@outlook.com>
2021-10-19 17:36:16 +0000

    Make game responsive for 768px

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,19 @@ const resultBox = document.querySelector(".result_box");
 const start_btn = document.getElementById("continue");
 const next_btn = quizBox.querySelector("next-btn");
 const option_list = document.querySelectorAll(".option");
-const clear_button = document.querySelector(".clear buttons");
-const replay_quiz = document.querySelector(".replay buttons");
+
+const clear_button = document.getElementById("clear-board");
+const replay_quiz = document.getElementById("replay")
 const exit_btn = document.getElementById("quit")
 
 const timeText = document.querySelector(".timer .time-passed");
 const timeCount = document.querySelector(".timer .timer-sec");
 
+const table_clear = document.querySelector('table > tbody');
+
 /* Initialsing / Declaring varibales needed for script */
 let counter;
+let que_numb = 1;
 
 start_btn.onclick = ()=>{
     flipcard.classList.add("hidden"); //hide flipcard
@@ -23,7 +27,42 @@ start_btn.onclick = ()=>{
 }
 
 exit_btn.onclick = ()=>{
-    resultBox.classList.add("hidden"); //remove hidden from quiz
-    flipcard.classList.remove("hidden"); //hide flipcard
+    resultBox.classList.add("hidden"); //hide results
+    flipcard.classList.remove("hidden"); //show flipcard
    
+}
+
+replay_quiz.onclick = ()=>{
+    resultBox.classList.add("hidden"); //hide results
+    quizBox.classList.remove("hidden"); //show flipcard
+
+    // add function which generates new questions 
+}
+
+clear_button.onClick = ()=>{
+
+}
+
+/**
+ * 
+ * This will randomly display the questions to the user
+ */
+function showQuestions(index){
+    
+}
+
+/**
+ * 
+ * This will check if the users answer is the correct one
+ */
+function optionSelected(answer){
+
+}
+
+/**
+ * 
+ * This will cause the timer to increase when the user starts the quiz 
+ */
+function startTimer(time){
+
 }
